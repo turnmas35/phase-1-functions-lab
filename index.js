e87667cf01f6c8661be03d3069150c5f54d22ace
Matthew Turner <turnmas35@gmail.com>
2023-09-09 15:31:00 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    const hqStreet = 42
+    return Math.abs(street - hqStreet);
+  }
+
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264;
+  }
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+  }
+
+function calculatesFarePrice(start, destination) {
+    const feetPerBlock = 264;
+    const distanceInFeet = Math.abs(start - destination) * feetPerBlock;
+    
+    if (distanceInFeet <= 400) {
+        return 0;
+    }   else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+        return (distanceInFeet - 400) * 0.02;
+    }   else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+        return 25;
+    }   else {
+        return 'cannot travel that far';
+    }
+  }
